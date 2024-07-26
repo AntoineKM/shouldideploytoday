6fc552f2df9c9d606f3e500c219d375608de3322
Antoine Kingue <contact@antoinek.fr>
2024-07-26 13:06:45 +0200

    💄 add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { KitchnProvider } from 'kitchn'
 import type { AppProps } from 'next/app'
 
+import "kitchn/fonts.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <KitchnProvider>
